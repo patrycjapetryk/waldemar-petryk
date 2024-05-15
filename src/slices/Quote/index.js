44d7e550fb31c6de8d90931eb166e6a27035a240
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-05-15 19:43:11 +0200

    Remove quotes

--- src/slices/Quote/index.js
+++ src/slices/Quote/index.js
@@ -1,19 +1,17 @@
-import * as prismic from "@prismicio/client";
-import { PrismicText } from "@prismicio/react";
+import * as prismic from '@prismicio/client';
+import { PrismicText } from '@prismicio/react';
 
-import { Bounded } from "@/components/Bounded";
+import { Bounded } from '@/components/Bounded';
 
 const Quote = ({ slice }) => {
   return (
-    <Bounded as="section" size="wide">
+    <Bounded as='section' size='wide'>
       {prismic.isFilled.richText(slice.primary.quote) && (
-        <div className="font-serif text-3xl italic leading-relaxed">
-          &ldquo;
+        <div className='font-serif text-3xl italic leading-relaxed'>
+          {/* &ldquo; */}
           <PrismicText field={slice.primary.quote} />
-          &rdquo;
-          {prismic.isFilled.keyText(slice.primary.source) && (
-            <> &mdash; {slice.primary.source}</>
-          )}
+          {/* &rdquo; */}
+          {prismic.isFilled.keyText(slice.primary.source) && <> &mdash; {slice.primary.source}</>}
         </div>
       )}
     </Bounded>
